a28dd3fedd2128bb635be0b05c3d48617703361e
IDOTxLION <66892855+IDOTxLION@users.noreply.github.com>
2023-04-18 11:10:28 -0400

    Update Combinaison.cs

--- Labo03-ProjetDépart/Combinaison.cs
+++ Labo03-ProjetDépart/Combinaison.cs
@@ -1,4 +1,4 @@
-﻿// Labo 03, H-2023
+// Labo 03, H-2023
 //
 // Classe pouvant retenir une combinaison d'une loterie dont le type
 // sera déterminé à la construction d'une classe dérivée
@@ -13,20 +13,59 @@ using System.Collections.Generic;
 
 namespace Labo03
 {
-   abstract class Combinaison
-   {
-      /// <summary>
-      /// Constructeur paramétrique
-      /// </summary>
-      public Combinaison(int nbChiffres, int min, int max)
-      {
-      }
+    abstract class Combinaison
+    {
+        int nbChiffres;
+        int min;
+        int max;
 
-      public override string ToString()
-      {
-         string chaine = string.Empty;
+        //public int NbChiffres
+        //{ get => nbChiffres; set { nbChiffres = value; } }
+        //public int Min
+        //{ get => min; set { min = value; } }
+        //public int Max
+        //{ get => max; set { max = value; } }
+        /// <summary>
+        /// Constructeur paramétrique
+        /// </summary>
+        public Combinaison(int nbChiffres, int min, int max)
+        {
+            this.nbChiffres = nbChiffres;
+            this.min = min;
+            this.max = max;
+        }
+        // 
+        public static List<int> D(int nbChiffres, int min, int max)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i != nbChiffres; i++)
+            {
+                list.Add(Générateur.Next(min, max));
+            }
+            return list;
 
-         return chaine;
-      }
-   }
+
+        }
+
+
+        public override string ToString()
+        {
+            List<int> list = D(nbChiffres, min, max);
+            string chaine = string.Empty;
+            //foreach (int n in D(nbChiffres, min, max))
+            //    if(n == D(nbChiffres, min, max).Count-1)
+            //        chaine += n.ToString();
+            //    else
+            //        chaine += string.Format(" {0} - ", n);
+            for (int i = 0; i < list.Count; i++)
+                if (i == list.Count - 1)
+                    chaine += list[i].ToString();
+                else
+                    chaine += string.Format(" {0} - ", list[i]);
+
+
+            return chaine;
+
+        }
+    }
 }
